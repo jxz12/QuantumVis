b134dca7be931ee450869a6fe2e478f6b181a05f
jz <jonathanzheng@hotmail.co.uk>
2019-11-20 10:12:34 +0000

    initial commit

---
+++ Assets/Gates/Gate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Gate : MonoBehaviour, IDropHandler
+{
+    public Complex[] input1, input2;
+    public Complex[] output1, output2;
+
+    public enum Type { PauliX, Hadamard, CNOT };
+    public Type top, bottom;
+
+    public void OnDrop(PointerEventData ped)
+    {
+
+    }
+}
