28509a6222a54c9cfe0e072b08a42382ad3ac9ad
jxz12 <jxz12@ic.ac.uk>
2020-01-31 21:34:12 +0000

    fixed snap

--- Assets/Gates/Circuit.cs
+++ Assets/Gates/Circuit.cs
@@ -44,6 +44,7 @@ public class Circuit : MonoBehaviour
 
         var pickup = ped.pointerDrag.GetComponent<Pickup>();
 
+        float snappedPosX;
         // check if hovered
         if (xViewport<0 || xViewport>1 || yViewport<0 || yViewport>1) {
             snappedSectionX = snappedSectionY = -1;
@@ -51,19 +52,43 @@ public class Circuit : MonoBehaviour
             pickup.Alpha = .7f;
             return;
         }
-        // snap
-        int nSlotsX = 2*gates.Count + 3;
-        snappedSectionX = (int)Mathf.Round(xViewport * (nSlotsX-1));
-        // sections at end can be snapped to one-from-end
-        snappedSectionX = Math.Min(nSlotsX-2, Math.Max(1, snappedSectionX));
+        if (gates.Count == 0)
+        {
+            snappedSectionX = 0;
+            snappedPosX = xMin + (xMax - xMin) / 2;
+        }
+        else // find closest insertion/replacement gate to snap to
+        {
+            // yes this is brute force but I am pragmatic
+            var xSnaps = new float[2*gates.Count + 1];
+            xSnaps[0] = (xMin + gates[0].transform.position.x) / 2;
+            xSnaps[1] = gates[0].transform.position.x;
+            for (int i=1; i<gates.Count; i++)
+            {
+                xSnaps[i*2] = (gates[i-1].transform.position.x + gates[i].transform.position.x) / 2;
+                xSnaps[i*2+1] = gates[i].transform.position.x;
+            }
+            xSnaps[2*gates.Count] = (gates[gates.Count-1].transform.position.x + xMax) / 2;
+
+            snappedSectionX = 0;
+            snappedPosX = xSnaps[0];
+            float maxDist = Mathf.Abs(xSnaps[0]-ped.position.x);
+            for (int i=0; i<xSnaps.Length; i++)
+            {
+                float dist = Mathf.Abs(xSnaps[i]-ped.position.x);
+                if (dist < maxDist)
+                {
+                    snappedSectionX = i;
+                    maxDist = dist;
+                    snappedPosX = xSnaps[i];
+                }
+            }
+        }
 
         // hard coded for two qubits only
         int nSlotsY = (pickup.type==Gate.Type.C || pickup.type==Gate.Type.NOT)? 1 : 2;
         snappedSectionY = (int)Mathf.Round(yViewport * (nSlotsY-1));
-
-        float snappedPosX = nSlotsX<=1? .5f : (float)snappedSectionX / (nSlotsX-1);
         float snappedPosY = nSlotsY==1? .5f : .25f + .5f*snappedSectionY / (nSlotsY-1);
-        snappedPosX = xMin + snappedPosX*(xMax-xMin);
         snappedPosY = yMin + snappedPosY*(yMax-yMin);
 
         pickup.transform.position = new Vector2(snappedPosX, snappedPosY);
@@ -80,22 +105,23 @@ public class Circuit : MonoBehaviour
             return;
         }
         bool topHalf = snappedSectionY==1;
-        if (snappedSectionX%2 == 1) // odd numbers mean new gate
+        if (snappedSectionX%2 == 0) // even numbers mean new gate
         {
             if (pickup.type == Gate.Type.Identity) {
                 return;
             }
             var newGate = Instantiate(gatePrefab, gatesLayout.transform);
-            int idx = (snappedSectionX-1) / 2;
+            int idx = snappedSectionX / 2;
             gates.Insert(idx, newGate);
             newGate.SetType(pickup.type, topHalf);
             if (pickup.type!=Gate.Type.C && pickup.type!=Gate.Type.NOT) {
                 newGate.SetType(Gate.Type.Identity, !topHalf); // initialise other half
             }
+            newGate.transform.SetSiblingIndex(idx);
         }   
         else // hovered over existing
         {
-            var hoveredGate = gates[snappedSectionX/2 - 1];
+            var hoveredGate = gates[(snappedSectionX-1) / 2];
             bool isIdentity = hoveredGate.SetType(pickup.type, topHalf);
             if (isIdentity)
             {
@@ -103,10 +129,10 @@ public class Circuit : MonoBehaviour
                 Destroy(hoveredGate.gameObject);
             }
         }
-        foreach (Gate gate in gates)
-        {
-            gate.transform.SetAsLastSibling();
-        }
+        // foreach (Gate gate in gates) // reorder if needed
+        // {
+        //     gate.transform.SetAsLastSibling();
+        // }
         float n = (float)gates.Count;
         float layoutBorder = n<=1? 0 : (1/(n+1) - 1/(2*n)) / (1 - 1/n);
         gatesLayout.padding.left = gatesLayout.padding.right = (int)(layoutBorder * xRange);
